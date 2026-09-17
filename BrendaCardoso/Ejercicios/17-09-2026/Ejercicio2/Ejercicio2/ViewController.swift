136aa4f6ecf8f25ef8c59007852d767283fbff3f
hexab1t3 <fer.uri.mi.ro@gmail.com>
2026-09-17 15:04:55 -0600

    Ejercicio 2

---
+++ BrendaCardoso/Ejercicios/17-09-2026/Ejercicio2/Ejercicio2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Ejercicio2
+//
+//  Created by MacBook 6 on 17/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func greetButtonCap(_ sender: Any) {
+    }
+}
+
