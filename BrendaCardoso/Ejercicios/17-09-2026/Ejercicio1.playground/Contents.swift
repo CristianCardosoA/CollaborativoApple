30e24c77a444210636924f1a252b818671e715e1
hexab1t3 <fer.uri.mi.ro@gmail.com>
2026-09-17 14:46:15 -0600

    Ejercicio1

--- BrendaCardoso/Ejercicios/17-09-2026/Ejercicio1.playground/Contents.swift
+++ BrendaCardoso/Ejercicios/17-09-2026/Ejercicio1.playground/Contents.swift
@@ -1,6 +1,13 @@
 import UIKit
 
-let userInputAge: String = "34e"
-
-let userAge: Int? = Int(userInputAge)
+func ejercicio(){
+    let userInputAge: String = "34e"
+    
+    if let userAge = Int(userInputAge){
+        print("la edad del usuario es \(userAge)")
+        
+    }else {
+        print("la edad no es valida")
+    }
+}
 
