0abcbe5f8d4f7f54c4c3cdae21933a0d415eb61d
Tellox12 <facultaddecontaduriayadministracion@MacBook-15.local>
2026-09-24 14:58:57 -0600

    Segues

---
+++ Tello/Ejercicios/24-09-2026/ejercicio 2 segues/ejercicio 2 segues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ejercicio 2 segues
+//
+//  Created by Facultad de Contaduría y Administración on 24/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
