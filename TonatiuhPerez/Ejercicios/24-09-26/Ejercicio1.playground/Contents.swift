cc6f9fa4438dc7b8dcd27e4de410519d51cf1091
Alexcast0 <tonatiuhperez1999@gmail.com>
2026-09-24 14:05:11 -0600

    Ejercicio 1 24 de sept

--- TonatiuhPerez/Ejercicios/24-09-26/Ejercicio1.playground/Contents.swift
+++ TonatiuhPerez/Ejercicios/24-09-26/Ejercicio1.playground/Contents.swift
@@ -1,14 +1,24 @@
 import UIKit
 
 let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var index = 0
 
-
-for index in 1...100{
+for index in 1...100 {
     print(index)
 }
 
-for (index, letter) in letras.enumerated(){
+for (index, letter) in letras.enumerated() {
     print("\(index) : \(letter)")
+}
+
+while index < letras.count {
+
+    let stringIndex = letras.index(letras.startIndex, offsetBy: index)
+    let letter = letras[stringIndex]
+    
+    print("\(index) : \(letter)")
+    
+    index += 1
 }
 
 let estados: [String : String] = [
