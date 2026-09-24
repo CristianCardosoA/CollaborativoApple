5e957bda471a0e97709dff040a60c0b3a6065ce5
hexab1t3 <fer.uri.mi.ro@gmail.com>
2026-09-24 14:58:15 -0600

    Ejercicio 2

---
+++ BrendaCardoso/Ejercicios/24-09-2026/Ejercicio2/Ejercicio2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio2
+//
+//  Created by MacBook 6 on 24/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
