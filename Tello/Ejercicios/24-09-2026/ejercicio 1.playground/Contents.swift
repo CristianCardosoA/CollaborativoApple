8440362d431600199484a30921b63e94be456c8f
Tellox12 <facultaddecontaduriayadministracion@MacBook-15.local>
2026-09-24 13:51:14 -0600

    ejercicio 1 for

---
+++ Tello/Ejercicios/24-09-2026/ejercicio 1.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+let statesAndCapitals: [String: String] = [
+    "Utah": "Salt Lake City",
+    "New York": "Albany",
+    "Virginia": "Richmond"
+]
+
+for letra in alphabet {
+    
+    print(letra)
+}
+
+
+for (estados) in statesAndCapitals {
+    print(estados)
+}
+    
