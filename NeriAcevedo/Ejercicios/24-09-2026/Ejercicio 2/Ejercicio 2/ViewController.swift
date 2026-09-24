6b8edf3e4458e70acfd297dae85006cc677fab18
Neribb <angelica.ortiz0606@gmail.com>
2026-09-24 14:30:51 -0600

    Agregar gitignore

---
+++ NeriAcevedo/Ejercicios/24-09-2026/Ejercicio 2/Ejercicio 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio 2
+//
+//  Created by Facultad de Contaduría y Administracíon on 24/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
