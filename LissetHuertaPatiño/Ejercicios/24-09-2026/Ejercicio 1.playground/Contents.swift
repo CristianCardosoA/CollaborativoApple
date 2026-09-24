c42d02b993aa4da7f4b5a8cbf12adcc45cb450f2
Lisset <Liss@gmail.com>
2026-09-24 14:04:21 -0600

    clase 24 sept

--- LissetHuertaPatiño/Ejercicios/24-09-2026/Ejercicio 1.playground/Contents.swift
+++ LissetHuertaPatiño/Ejercicios/24-09-2026/Ejercicio 1.playground/Contents.swift
@@ -1,7 +1,13 @@
 import UIKit
 
-for index in 1...100{
+for index in 1...100 {
     print(index)
+}
+
+var number = 1
+while number <= 100 {
+    print(number)
+    number += 1
 }
 
 let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -9,6 +15,14 @@ let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 for(index, letter) in alphabet.enumerated(){
     print("\(index): \(letter)")
 }
+
+let alphabetArray = Array(alphabet)
+var index = 0
+while index < alphabetArray.count {
+    print("\(index): \(alphabetArray[index])")
+    index += 1
+}
+
 
 let statesAndCapitals: [String: String] = [
     "Utah": "Salt Lake City",
@@ -18,4 +32,17 @@ let statesAndCapitals: [String: String] = [
 
 for (state, capital) in statesAndCapitals {
     print("The capital of \(state) is \(capital).")
+}
+
+
+let parejas = Array(statesAndCapitals)
+
+var i = 0
+
+while i < parejas.count {
+    let state = parejas[i].key
+    let capital = parejas[i].value
+    
+    print("The capital of \(state) is \(capital).")
+    i += 1
 }
